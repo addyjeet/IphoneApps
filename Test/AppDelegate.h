6bd6fd9d2ed169190c8b83cc967b5254e3ab8de6
addy <addyjeet@alumni.cmu.edu>
2014-03-06 23:15:40 -0600

    Initial commit from local git

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Addy on 3/6/14.
+//  Copyright (c) 2014 Addy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Addy on 3/6/14.
+//  Copyright (c) 2014 Addy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
